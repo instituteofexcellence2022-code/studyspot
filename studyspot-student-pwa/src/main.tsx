c0349b53f41b66ef3e4d1dcd87d5817337831593
Institute of Excellence <instituteofexcellence2022@gmail.com>
2025-11-03 15:42:34 +0530

    fix: disable PWA service worker for dev, add index.css

--- studyspot-student-pwa/src/main.tsx
+++ studyspot-student-pwa/src/main.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { registerSW } from 'virtual:pwa-register'
 
-// Register Service Worker for PWA
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('New version available! Reload to update?')) {
-      updateSW(true)
-    }
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline')
-  },
-})
+// PWA Service Worker registration (disabled for dev)
+// import { registerSW } from 'virtual:pwa-register'
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     if (confirm('New version available! Reload to update?')) {
+//       updateSW(true)
+//     }
+//   },
+//   onOfflineReady() {
+//     console.log('App ready to work offline')
+//   },
+// })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
